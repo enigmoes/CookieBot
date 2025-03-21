8d41627a71848ee346673694ca9c18c87ccc1717
Enigmo <enigmo@enigmo.es>
2025-03-21 13:06:07 +0100

    Add Dark and Light Mode

---
+++ CookieBot Extension/Resources/colorMode.js
@@ -0,0 +1,75 @@
+(() => {
+    'use strict'
+
+    const getStoredTheme = () => localStorage.getItem('theme')
+    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = theme => {
+        if (theme === 'auto') {
+            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+    }
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector('#bd-theme')
+
+        if (!themeSwitcher) {
+            return
+        }
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.remove('active')
+            element.setAttribute('aria-pressed', 'false')
+        })
+
+        let themeValue = 'auto'
+
+        if (theme == 'dark') {
+            themeValue = 'light'
+        } else if (theme == 'light') {
+            themeValue = 'dark'
+        }
+
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${themeValue}"]`)
+        btnToActive.classList.add('active')
+        btnToActive.setAttribute('aria-pressed', 'true')
+
+        if (focus) {
+            themeSwitcher.focus()
+        }
+    }
+
+    setTheme(getPreferredTheme())
+    showActiveTheme(getPreferredTheme())
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(toggle => {
+            toggle.addEventListener('click', () => {
+                const theme = toggle.getAttribute('data-bs-theme-value')
+                setStoredTheme(theme)
+                setTheme(theme)
+                showActiveTheme(theme, true)
+            })
+        })
+    })
+})()
